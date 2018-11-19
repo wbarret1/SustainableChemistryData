674645f1bcd191d73f7ee38dd94cf18cbe1b4627
wbarret1 <barrett.williamm@epa.gov>
2018-11-19 15:40:16 -0500

    Add project files.

---
+++ SustainableChemistryData/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\WBARRETT\Source\Repos\SustainableChemistryData\SustainableChemistryData\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
